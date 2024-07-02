992a25feaeb83a96b814ae26a074694fa1260667
Martin Schulz <4martin.schulz@aikq.de>
2024-07-02 10:08:42 -0300

    Create class DLinks to encapsulate the data.

--- cpp/sudoku_solve.cpp
+++ cpp/sudoku_solve.cpp
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include "dlinks_matrix.hpp"
 
-
 //accepts up to 2 arguments: 
 //  1 - file of puzzles, puzzles are a new-line delimited string of numbers with 0 representing empty boxes
 //      defaults to "puzzles.txt"
 //  2 - output file, solution will be written to the output file in the same format as the input file
 //      defaults to "solutions.txt"
 int main(int argc, char *argv[]){
-    char solution[83];
     char puzzle[83];
     FILE* puzzle_file;
     FILE* solutions_file;
@@ -18,12 +16,16 @@ int main(int argc, char *argv[]){
     if(argc > 2) { solutions_file = fopen(argv[2], "w"); }
     else { solutions_file = fopen("solutions.txt", "w"); }
 
+	DLinks *dl = new DLinks;
+
     while(fgets(puzzle, 83, puzzle_file) != 0){
-        if(solve_puzzle(puzzle)) {
+	    char solution[83];
+        if(solve_puzzle(dl, puzzle)) {
             int index, value;
             for(int i=0; i<81; i++){
-                index = solution_stack[i]->row / 9;
-                value = (solution_stack[i]->row % 9) + '1';
+				int row = dl->solution_stack[i]->row;
+                index = row / 9;
+                value = (row % 9) + '1';
                 solution[index] = value;
             }
         }
